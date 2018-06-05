c11de4a893ed62f9a859696893f2e8e3cea249f9
jsw7524 <jsw7524@gmail.com>
2018-06-05 10:24:59 +0800

    AC

--- 811. Subdomain Visit Count/Program.cs
+++ 811. Subdomain Visit Count/Program.cs
@@ -6,6 +6,43 @@ using System.Threading.Tasks;
 
 namespace _811.Subdomain_Visit_Count
 {
+    public class Solution
+    {
+        private Dictionary<string, int> dic;
+        public IList<string> SubdomainVisits(string[] cpdomains)
+        {
+            dic = new Dictionary<string, int>();
+            foreach (string cpd in cpdomains)
+            {
+                var tmp = cpd.Split(' ');
+                int number = int.Parse(tmp[0]);
+                string domain = tmp[1];
+                string subDomain = "";
+                foreach (var s in domain.Split('.').Reverse())
+                {
+                    if ("" == subDomain)
+                    {
+                        subDomain = s;
+                    }
+                    else
+                    {
+                        subDomain = s + "." + subDomain;
+                    }
+
+                    if (dic.ContainsKey(subDomain))
+                    {
+                        dic[subDomain] = dic[subDomain]+number;
+                    }
+                    else
+                    {
+                        dic.Add(subDomain, number);
+                    }
+                }
+            }
+            return dic.Select(kp => kp.Value + " " + kp.Key).ToList();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _811.Subdomain_Visit_Count;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution sln = new Solution();
+            string[] testData = { "9001 discuss.leetcode.com" };
+            var result=sln.SubdomainVisits(testData);
+            IList<string> ans = new List<string>() { "9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com" };
+            Assert.AreEqual(Newtonsoft.Json.JsonConvert.SerializeObject(result.OrderBy(r=>r)), Newtonsoft.Json.JsonConvert.SerializeObject(ans.OrderBy(a=> a)));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution sln = new Solution();
+            string[] testData = { "900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org" };
+            var result = sln.SubdomainVisits(testData);
+            IList<string> ans = new List<string>() { "901 mail.com", "50 yahoo.com", "900 google.mail.com", "5 wiki.org", "5 org", "1 intel.mail.com", "951 com" };
+            Assert.AreEqual(Newtonsoft.Json.JsonConvert.SerializeObject(result.OrderBy(r => r)), Newtonsoft.Json.JsonConvert.SerializeObject(ans.OrderBy(a => a)));
+        }
+    }
+}
